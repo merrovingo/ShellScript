f3a3cb6dc5fc89496ed98cb69e04e5bdb3df9e89
Enrique Tezozomoc <merrovingo@gmail.com>
2014-02-16 00:42:47 -0600

    Cambios en Agenda
    Agregada la función de búsqueda, falta validar existencia de un archivo
    agenda previamente creado

--- agenda.sh
+++ agenda.sh
@@ -10,16 +10,15 @@ function nuevos_registros(){
 	clear
 	echo "Ingresa tu nombre:"
 	read NOMBRE
-	echo "$NOMBRE" >> $AGENDA
 	echo "Ingresa tu numero de telefono:"
 	read TELEFONO
-	echo "$TELEFONO" >> $AGENDA
 	echo "Ingresa tu fecha de nacimiento:"
 	read NACIMIENTO
-	echo "$NACIMIENTO" >> $AGENDA
 	echo "Ingresa tu direccion de correo electronico:"
 	read MAIL
-	echo "$MAIL" >> $AGENDA
+	echo "Nombre: $NOMBRE 	Telefono: $TELEFONO 	\
+	Fecha de nacimiento: $NACIMIENTO 	\
+	Correo electronico: $MAIL" >> $AGENDA
 	echo "Datos almacenados correctamente en $AGENDA"
 	sh agenda.sh
 }
@@ -30,6 +29,52 @@ function nueva_agenda(){
 	touch $AGENDA
 	echo "Agenda creada correctamente"
 	sh agenda.sh
+}
+function nueva_busqueda(){
+	clear
+	echo "Busqueda de registros"
+	echo "Elige una opcion:"
+	echo "1 - Buscar por nombre"
+	echo "2 - Buscar por telefono"
+	echo "3 - Buscar por fecha de nacimiento"
+	echo "4 - Buscar por correo electronico"
+	echo "5 - Salir"
+	read RESPUESTA_BUSQUEDA
+	case $RESPUESTA_BUSQUEDA in
+		1) echo "Ingresa el nombre:"
+		read BUSQUEDA_NOMBRE
+		grep $BUSQUEDA_NOMBRE ~/$AGENDA
+		echo "Presiona cualquier tecla para continuar..."
+		read pausa
+		nueva_busqueda()
+		;;
+		2) echo "Ingresa el numero de telefono:"
+		read $BUSQUEDA_TELEFONO
+		grep $BUSQUEDA_TELEFONO ~/$AGENDA
+		echo "Presiona cualquier tecla para continuar..."
+		read pausa
+		nueva_busqueda()
+		;;
+		3) echo "Ingresa la fecha de nacimiento:"
+		read BUSQUEDA_FECHA
+		grep $BUSQUEDA_FECHA ~/$AGENDA
+		echo "Presiona cualquier tecla para continuar..."
+		read pausa
+		nueva_busqueda()
+		;;
+		4) echo "Ingresa la direccion de correo:"
+		read BUSQUEDA_MAIL
+		grep $BUSQUEDA_MAIL ~/$AGENDA
+		echo "Presiona caulquier tecla para continuar..."
+		read pausa
+		nueva_busqueda()
+		;;
+		5) echo "Nos vemos..."
+		;;
+		*) echo "Selecciona una opcion valida"
+		nueva_busqueda()
+		;;
+	esac
 }
 #########################################################
 #		Main				#
@@ -48,8 +93,9 @@ case $RESPUESTA in
 	;;
 	2) nuevos_registros()
 	;;
+	3) nueva_busqueda()
+	;;
 	4) echo "Nos vemos..."
-	exit
 	;;
 	*) echo "Opcion no valida, intentalo nuevamente"
 	sh agenda.sh
